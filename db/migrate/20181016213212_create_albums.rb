33c49b1ba132e3e096ca293666da87bfcb562ac5
swgoodman <spencerwgoodman@icloud.com>
2018-10-16 14:59:57 -0700

    Established Album attributeds

--- db/migrate/20181016213212_create_albums.rb
+++ db/migrate/20181016213212_create_albums.rb
@@ -1,7 +1,10 @@
 class CreateAlbums < ActiveRecord::Migration[5.2]
   def change
     create_table :albums do |t|
-
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+      t.integer :rank
       t.timestamps
     end
   end
