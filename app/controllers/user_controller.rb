99e9322633a9b32a569388ada06494a905983c03
swgoodman <spencerwgoodman@icloud.com>
2018-10-16 17:05:53 -0700

    Set up new User login form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,20 @@
 class UserController < ApplicationController
 
+  before_action :current_user, only: [:index]
+  
   def index
   end
 
   def create
   end
 
+
+private
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      @user = User.new
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   has_many :albums
   has_many :artists, through: :albums
   has_many :genres, through: :albums
