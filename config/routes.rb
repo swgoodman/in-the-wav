ba8c46d3f1fc5ccf7f9aa3c127f24ff4648f78c7
swgoodman <spencerwgoodman@icloud.com>
2018-10-17 16:06:07 -0700

    Added nested Albums routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
-  resources :users, :only => [:create, :show]
+  resources :users, :only => [:create, :show] do
+    resources :albums, :only => [:index, :show, :update, :destroy]
+  end
 
   get '/auth/spotify/callback' => 'sessions#create'
   post '/sessions', to: 'sessions#create'
