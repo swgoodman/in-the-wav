5f9ce7eb9e3e4999def922e577f5f0e44653f48f
swgoodman <spencerwgoodman@icloud.com>
2018-10-16 23:36:14 -0700

    Added strong User params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.build(user_params)
   end
 
 
 private
+
   def current_user
     if session[:user_id]
       @user = User.find(session[:user_id])
@@ -17,4 +19,9 @@ private
       @user = User.new
     end
   end
+
+  def user_params
+    params.require[:user].permit[:username, :email, :password]
+  end
+
 end
