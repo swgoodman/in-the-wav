52302ba4c7f086e285d3e4a490c9038e534c08c8
swgoodman <spencerwgoodman@icloud.com>
2018-10-17 00:14:10 -0700

    Added session controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   resources :users, :only => [:index, :create, :show]
