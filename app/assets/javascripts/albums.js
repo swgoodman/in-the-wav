d0806ac6cb3bb898455740551ab4148f96fca826
swgoodman <spencerwgoodman@icloud.com>
2018-10-18 15:36:29 -0700

    API results loading with AJAX

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -14,8 +14,29 @@ $(function() {
       beforeSend: function(request) {
         request.setRequestHeader('Authorization', 'Bearer ' + accessToken);
       }
-    }).done(function(response) {
-      debugger
+    }).success(function(response) {
+      var $search_list = $("div#search_results ol")
+      $search_list.html("")
+
+        $.each(response.albums.items, function(name, value) {
+          console.log(value.artists[0].name);
+          $search_list.append("<li>" + value.name + '-' + value.artists[0].name + "</li>");
+          console.log(value.name);
+          console.log(value.release_date);
+          console.log(value.external_urls.spotify);
+          console.log(value.images[0].url);
+        })
+        debugger
+    }).error(function(notNeeded){
+      alert("Error, please try again. If error persists, please log out and back in again.")
     });
+
+    // $.ajax({
+    //   url: this.href,
+    //   dataType: 'script'
+    // })
+    // })
   });
+
+
 });
