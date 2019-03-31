208b183fe818230a40c61731a25a5791a7e34c44
swgoodman <spencerwgoodman@icloud.com>
2019-03-31 16:01:19 -0700

    Adding function comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
 
+  # Verify user before showing single page of web app
   def home
     if current_user
       redirect_to user_albums_path(@user)
@@ -10,10 +11,11 @@ class StaticPagesController < ApplicationController
 
   private
 
-    def current_user
-      if session[:user_id]
-        @user = User.find(session[:user_id])
-      end
+  # Find and set current user
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
     end
+  end
 
 end
