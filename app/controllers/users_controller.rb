e69f8d06453e82b6b540674c6dbd8b258a277c4f
swgoodman <spencerwgoodman@icloud.com>
2019-03-31 16:04:53 -0700

    Adding function comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authentication_required, :except => [:create]
 
+  # Create new user
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,14 +12,15 @@ class UsersController < ApplicationController
     end
   end
 
+  # Show 
   def show
     @user = User.find(params[:id])
-    render json: @album
+    render json: @user
   end
-
 
 private
 
+  # Accpetable user params
   def user_params
     params.require(:user).permit(:username, :email)
   end
