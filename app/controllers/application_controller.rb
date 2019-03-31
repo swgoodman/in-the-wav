f55a8325e68e99bbbd91e68952020dcfa3a1415c
swgoodman <spencerwgoodman@icloud.com>
2019-03-31 15:59:04 -0700

    Adding function comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,21 @@
 class ApplicationController < ActionController::Base
 
+  # Spotify API connection gem
   RSpotify::authenticate(ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'])
 
+  # User log in verification
   def authentication_required
     if !logged_in?
       redirect_to root_path
     end
   end
 
+  # Logged In Check
   def logged_in?
     !!current_user
   end
 
+  # Return current user helper method
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
